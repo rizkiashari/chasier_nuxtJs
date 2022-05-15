793b541853b494aa6ab82cdc34019c89aa288d7d
RizkiAshari <rizkiashari1026@gmail.com>
2022-05-15 20:18:17 +0700

    Initial Project and Filter Product

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row>
+    <v-col cols="8" class="frame-content px-6"> <products></products> </v-col>
+    <v-col cols="4" class="frame-content white"> ORDERS </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
