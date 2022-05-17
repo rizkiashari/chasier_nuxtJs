08857c5dc45e43ab75d4fe399278bda556bb57b8
RizkiAshari <rizkiashari1026@gmail.com>
2022-05-17 22:04:08 +0700

    Connect to mongoodb

--- BE/app.js
+++ BE/app.js
@@ -1,16 +1,27 @@
 import express from "express";
-import logger from "morgan";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 import indexRouter from "./routes/index.js";
 
 const env = dotenv.config().parsed;
 var app = express();
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/", indexRouter);
+
+// Connect to mongodb
+mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`, {
+  dbName: env.MONGODB_DB_NAME,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
 
 app.listen(env.APP_PORT, () => {
   console.log(`Server started on port ${env.APP_PORT}`);
