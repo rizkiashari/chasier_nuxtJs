dd5afaba2bf99fdc02169d893c91b89a1578e8a1
RizkiAshari <rizkiashari1026@gmail.com>
2022-05-17 21:50:44 +0700

    Add Env

--- BE/app.js
+++ BE/app.js
@@ -1,9 +1,10 @@
 import express from "express";
-import path from "path";
 import logger from "morgan";
+import dotenv from "dotenv";
 
 import indexRouter from "./routes/index.js";
 
+const env = dotenv.config().parsed;
 var app = express();
 
 app.use(logger("dev"));
@@ -11,6 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(env.APP_PORT, () => {
+  console.log(`Server started on port ${env.APP_PORT}`);
 });
