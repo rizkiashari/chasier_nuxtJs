267e756cd9c5c06ddcfd4f29161352279f675ca4
RizkiAshari <rizkiashari1026@gmail.com>
2022-05-21 11:19:41 +0700

    Set Cross Origin in Header

--- BE/app.js
+++ BE/app.js
@@ -7,6 +7,15 @@ import morgan from "morgan";
 const env = dotenv.config().parsed;
 var app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
